a454ad4d612f09b0b08e421f6d40767db980a639
Paweł Wójcik <wojcik.pawel89@gmail.com>
2022-12-13 20:44:14 +0100

    Add assertJ examples

---
+++ src/test/java/com/sda/AssertionsExamplesAssertJ.java
@@ -0,0 +1,34 @@
+package com.sda;
+
+import com.sda.person.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AssertionsExamplesAssertJ {
+    @Test
+    void assertionsExamples() {
+        Adder adder = new Adder();
+        Integer result = adder.add(2, 3);
+        assertThat(result).isEqualTo(5);
+        assertThat(result == 5).isTrue();
+        assertThat(result == 6).isFalse();
+        assertThat(result).isNotEqualTo(6);
+        assertThat(result).isNotNull();
+        Person person = null;
+        assertThat(person).isNull();
+
+        assertThat(Arrays.asList(1, 2, 3))
+                .hasSize(3)
+                .containsExactly(1, 2, 3);
+
+        assertThat("pawel")
+                .startsWith("p")
+                .endsWith("l");
+
+
+
+    }
+}
