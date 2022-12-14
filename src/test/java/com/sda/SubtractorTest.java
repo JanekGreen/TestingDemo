2be94f61a3424c7425603f523924f0f150f84726
Pawel Wojcik <pawel.wojcik@intive.com>
2022-12-14 19:44:21 +0100

    Added SubtractorTest

---
+++ src/test/java/com/sda/SubtractorTest.java
@@ -0,0 +1,17 @@
+package com.sda;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SubtractorTest {
+    @Test
+    void subtractorShouldSubtractNumbers(){
+        //given
+        Subtractor subtractor = new Subtractor();
+        //when
+       int result = subtractor.subtract(5, 3);
+       //then
+        Assertions.assertEquals(2, result);
+
+    }
+}
