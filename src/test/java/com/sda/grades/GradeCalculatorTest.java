da85dcefa95b29077b6b3817f9dd34f8864f9b5a
Pawel Wojcik <pawel.wojcik@intive.com>
2022-12-15 20:08:15 +0100

    GradeCalculatorTest cd

--- src/test/java/com/sda/grades/GradeCalculatorTest.java
+++ src/test/java/com/sda/grades/GradeCalculatorTest.java
@@ -1,8 +1,7 @@
 package com.sda.grades;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class GradeCalculatorTest {
     /*
@@ -13,5 +12,56 @@ class GradeCalculatorTest {
      * 49  - 0  % ndst
      * */
 
+    @Test
+    void shouldReturnBdb() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(90);
+        Assertions.assertEquals("bdb", result);
+    }
+    @Test
+    void shouldReturnDb() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(79);
+        Assertions.assertEquals("db", result);
+    }
+    @Test
+    void shouldReturnDstCeil() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(78);
+        Assertions.assertEquals("dst", result);
+    }
+    @Test
+    void shouldReturnDstFloor() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(69);
+        Assertions.assertEquals("dst", result);
+    }
+    @Test
+    void shouldReturnDop() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(50);
+        Assertions.assertEquals("dop", result);
+    }
+
+    @Test
+    void shouldReturnNdst() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(49);
+        Assertions.assertEquals("ndst", result);
+    }
+
+    @Test
+    void shouldReturnErrorIfPercentageAbove100() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(101);
+        Assertions.assertEquals("Error. Percentage out of range!", result);
+    }
+
+    @Test
+    void shouldReturnErrorIfPercentageBelow0() {
+        GradeCalculator gradeCalculator = new GradeCalculator();
+        String result = gradeCalculator.calculate(-1);
+        Assertions.assertEquals("Error. Percentage out of range!", result);
+    }
 
 }
